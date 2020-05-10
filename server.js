649b1fb453a90cf33b64a329b80ad7aeeef741d9
mohammed mustafa <karamillamustafa@gmail.com>
2020-05-10 05:32:45 +0200

    refactor

--- server.js
+++ server.js
@@ -5,20 +5,15 @@ const app = express();
 // ==============
 // AdverTising Scripts
 // =========================
-const curiosityAdCopyScripts = require("./routes/advertising-scripts/curiosity-ad-copy-scripts");
-const facebookNewsfeedAdScripts = require("./routes/advertising-scripts/facebook-newsfeed-ad-scripts");
-const ppcAdScripts = require("./routes/advertising-scripts/ppc-ad-scripts");
-const stealthCloseScripts = require("./routes/advertising-scripts/stealth-close-scripts");
+const advertisingScripts = require("./routes/advertising-scripts");
 // ============================================================================================
 // bulletScripts
 // ================
-const brunsonBulletScripts = require("./routes/bullet-scripts/brunson-bullet-scripts");
-const featureBenefitMeaningFbmBulletScript = require("./routes/bullet-scripts/feature-benefit-meaning-fbm-bullet-script");
+const bulletScripts = require("./routes/bullet-scripts");
 // ===========================================================================================================
 // caseStudiesTestimonialsScripts
 // ==================================
-const caseStudyScript = require("./routes/case-studies-testimonials-scripts/case-study-script");
-const millionDollarTestimonialScript = require("./routes/case-studies-testimonials-scripts/million-dollar-testimonial-script");
+const caseStudyScript = require("./routes/case-studies-testimonials-scripts");
 // ===================================================================================================================================
 // ContentCreationScripts
 // ===========================
@@ -147,21 +142,20 @@ app.get("/", (req, res) => {
 // AdverTising Scripts
 // =========================
 
-app.use("/", curiosityAdCopyScripts);
-app.use("/", facebookNewsfeedAdScripts);
-app.use("/", ppcAdScripts);
-app.use("/", stealthCloseScripts);
+app.use("/", advertisingScripts);
+// app.use("/", facebookNewsfeedAdScripts);
+// app.use("/", ppcAdScripts);
+// app.use("/", stealthCloseScripts);
 // ============================================================================================
 // bulletScripts
 // ================
-app.use("/", brunsonBulletScripts);
-app.use("/", featureBenefitMeaningFbmBulletScript);
+app.use("/", bulletScripts);
+// app.use("/", featureBenefitMeaningFbmBulletScript);
 
 // ===========================================================================================================
 // caseStudiesTestimonialsScripts
 // ==================================
 app.use("/", caseStudyScript);
-app.use("/", aboutMeScript);
 
 // ===================================================================================================================================
 // ContentCreationScripts
